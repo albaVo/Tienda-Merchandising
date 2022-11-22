cec3845327e675299c9565ed501b1a918e9c3d59
DESKTOP-AMN55HB\alba1 <alba12vo@gmail.com>
2022-11-22 21:08:52 +0100

    Entidad cliente

--- pTienda/api-tienda/src/modules/clientes/entities/cliente.entity.ts
+++ pTienda/api-tienda/src/modules/clientes/entities/cliente.entity.ts
@@ -1,1 +1,16 @@
-export class Cliente {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Cliente {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column('text', { unique: true })
+  nombre: string;
+
+  @Column('text', { unique: true })
+  telefono: string;
+
+  @Column('text', { unique: true })
+  direccion: string;
+}
